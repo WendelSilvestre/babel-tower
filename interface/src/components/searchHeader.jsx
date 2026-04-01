3c532826bff6755c620e241513b29a94376ba3e9
WendelSilvestre <wendelsilvestre2003@outlook.com>
2026-04-01 18:31:20 -0300

    Add section header and spaces between

--- interface/src/components/searchHeader.jsx
+++ interface/src/components/searchHeader.jsx
@@ -22,21 +22,32 @@ export function RouteSelection() {
   }
 
   return (
-    <header className="header">
-      <div className="container-input">
-        <input 
-          className="search-input"
-          type="text"
-          list="routes" 
-          value={inputValue} 
-          onChange={handleChange}>
-        </input>
-          <datalist id="routes">
-            {options.map(option => 
-                <option key={option.value} value={option.value}>{option.keyword}</option>)
-            }
-          </datalist>
-      </div>
+    <header className="header">   
+      <section className="header-container-section">
+        <div className="header-title-icon">
+          <a href="/">
+            <img alt="babel-tower" src="./src/assets/babel-icon.png" width="40vw" height="45vh"></img>
+            <p>Babel Tower</p>
+          </a>
+        </div>
+        <div className="container-input">
+          <input 
+            className="search-input"
+            type="text"
+            list="routes" 
+            value={inputValue} 
+            onChange={handleChange}>
+          </input>
+            <datalist id="routes">
+              {options.map(option => 
+                  <option key={option.value} value={option.value}>{option.keyword}</option>)
+              }
+            </datalist>
+        </div>
+        <div className="header-profile">
+          <p>Profile</p>
+        </div>
+      </section>   
     </header>
   )
 }
