811f81f33362a23ab5a07d7206f9183b8cc2a9f5
WendelSilvestre <wendelsilvestre2003@outlook.com>
2026-04-17 00:54:23 -0300

    Add allow host in vite.config

--- interface/vite.config.mjs
+++ interface/vite.config.mjs
@@ -12,7 +12,8 @@ export default defineConfig({
   },
   preview: {
     host: '0.0.0.0',
-    port: parseInt(process.env.PORT) || 4173,
+    port: parseInt(process.env.PORT) || 5173,
     strictPort: true,
+    allowedHosts: 'all',
   },
 })
