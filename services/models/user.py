68bb34ddbe5c30eba49f81e4c0b9f29860aca76e
WendelSilvestre <wendelsilvestre2003@outlook.com>
2026-04-23 17:22:24 -0300

    Add new user schema

--- services/models/user.py
+++ services/models/user.py
@@ -1,11 +1,17 @@
 from sqlalchemy import Column, Integer, String
 from database.connection import Base
+import random
+import string
+
+
+def generate_id():
+    return ''.join(random.choices(string.digits, k=16))
 
 
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(String, primary_key=True, default=generate_id, index=True)
     name = Column(String, index=True)
     email = Column(String, unique=True, index=True)
     password = Column(String)
